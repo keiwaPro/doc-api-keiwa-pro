467459af5e675328cb4ee5ff81919d011aa0963e
Kpalaiso <djahabertrand@gmail.com>
2024-08-19 03:42:09 +0000

    ajout d'un nouvel attribute

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'keiwaPro', // Usually your GitHub org/user name.
-  projectName: 'keiwaPro/doc-api-keiwa-pro', // Usually your repo name.
+  projectName: 'doc-api-keiwa-pro', // Usually your repo name.
 
   deploymentBranch:'main',
 
